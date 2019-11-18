ad0e64d5ab3d2f5212dbcbdbd82a9e4123d95395
Sarah Mathew <mathewsa@ornl.gov>
2019-11-18 12:41:31 -0500

    added function to convert images to dds: issue # 4

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from wand import Image
 # Create your views here.
+
+def convertImgToDds(img):
+    imgName = img[:img.rfind(".")]
+    with image.Image(filename=img):
+        img.compression = "dxt3"
+        img.save(filename=imgName+".dds")
