d7e46f43761a7574b0b8f68cd86c99eb04a65249
Sarah Mathew <sarahannmathew@gmail.com>
2019-12-10 20:50:02 +0000

    small update to dds function - mainly to test branch push

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from wand import Image
 # Create your views here.
+import os
 
-def convertImgToDds(img):
+def convertImgToDds(img,path):
     imgName = img[:img.rfind(".")]
     with image.Image(filename=img):
         img.compression = "dxt3"
-        img.save(filename=imgName+".dds")
+        img.save(os.path.join(path,imgName+".dds"))
