e48ce7aa1e26e0c1cad2e41996ad41086a20bdba
Sarah Mathew <mathewsa@ornl.gov>
2019-12-27 15:44:19 -0500

    added local host address to allowed host list

--- DataVizTrials/settings.py
+++ DataVizTrials/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = keyFile.readline()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vr.uncw.edu']
+ALLOWED_HOSTS = ['vr.uncw.edu', 'localhost', '127.0.0.1']
 
 
 # Application definition
