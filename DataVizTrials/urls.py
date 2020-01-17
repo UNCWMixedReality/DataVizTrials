9f87c7c0808dfefa37af005d376dc987f9b7193f
Sarah Mathew <mathewsa@ornl.gov>
2020-01-17 14:42:12 -0500

    fixed path typo

--- DataVizTrials/urls.py
+++ DataVizTrials/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('users', include('users.urls')),
-    path('tasks', include('tasks.urls')),
+    path('users/', include('users.urls')),
+    path('tasks/', include('tasks.urls')),
     path('admin/', admin.site.urls),
 ]
