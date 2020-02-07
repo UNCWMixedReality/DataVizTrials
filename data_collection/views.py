360f28fbf9469bce17d28af33ca3eb1e7c877044
Sarah Mathew <mathewsa@ornl.gov>
2020-02-07 12:23:59 -0500

    fixed bug in retrieiving database records

--- data_collection/views.py
+++ data_collection/views.py
@@ -110,8 +110,8 @@ def addTrialToTable(form):
 def chooseRandomTask(user_id):
     all_task_objects = TaskData.objects.all()
     all_task_ids = [task.task_id for task in all_task_objects]
-    if checkRecordExistence(TrialData, {"user_id":user_id}): 
-        chosen_task_objects = TrialData.objects.filter("user_id":user_id)
+    if checkRecordExistence(TrialData, {user_id:user_id}): 
+        chosen_task_objects = TrialData.objects.filter(user_id=user_id)
         chosen_task_ids = [task.task_id for task in chosen_task_objects]
         available_task_ids = list(set(all_task_ids)-set(chosen_task_ids))
         return random.choice(available_task_ids)
